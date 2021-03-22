64b5f2fc19bbbd64a20837ab59b8055e0a2e5bdd
Alan.chambaz <alan.chambaz@he-arc.ch>
2021-03-22 16:32:34 +0100

    Add stream url to tournament

--- backend/api/models/tournamentmodel.py
+++ backend/api/models/tournamentmodel.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 
-class Tournament(models.Model):
-	organizer = models.ForeignKey(User, to_field = "username", on_delete=models.CASCADE)
-	name = models.CharField(max_length=250)
-	gameName = models.CharField(max_length=250)
-	matchDuration = models.IntegerField()
-	breakDuration = models.IntegerField()
-	deadLineDate = models.DateField()
-	nbTeam = models.IntegerField()
 
-	def __str__(self):
-		return self.name
+class Tournament(models.Model):
+    organizer = models.ForeignKey(
+        User, to_field="username", on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    gameName = models.CharField(max_length=250)
+    matchDuration = models.IntegerField()
+    breakDuration = models.IntegerField()
+    deadLineDate = models.DateField()
+    nbTeam = models.IntegerField()
+    streamURL = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
