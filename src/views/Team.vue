f8f195fe7c08d7566865ad28c8a860f2331e1827
sebastie.roso <sebastien.roso@he-arc.ch>
2021-03-29 15:53:54 +0200

    test rebase

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -77,7 +77,7 @@
       <v-col xs="12" md="8">
         <v-card tile>
           <v-card-title color="#01002a"
-            >Membres</v-card-title
+            >Memmmmmbres</v-card-title
           >
           <v-list three-line>
             <template v-for="(item, index) in players">
