cecc2eac52b02dd715670786531b956c48e3356e
colin.pelletie <colin.pelletier@he-arc.ch>
2021-04-12 14:10:34 +0200

    Remove hard coded teams

--- src/views/TournamentBracket.vue
+++ src/views/TournamentBracket.vue
@@ -12,43 +12,11 @@ import MatchService from '@/services/MatchService'
 import TournamentService from '@/services/TournamentService'
 import TeamService from '@/services/TeamService'
 
-// const teams = [
-//   {
-//     id: 1,
-//     name: 'A'
-//   },
-//   {
-//     id: 2,
-//     name: 'B'
-//   },
-//   {
-//     id: 3,
-//     name: 'C'
-//   },
-//   {
-//     id: 4,
-//     name: 'D'
-//   },
-//   {
-//     id: 5,
-//     name: 'E'
-//   },
-//   {
-//     id: 6,
-//     name: 'F'
-//   },
-//   {
-//     id: 7,
-//     name: 'G'
-//   }
-// ]
-
 export default {
   components: {
     Bracket
   },
   mounted() {
-    // this.GetMatchesbyTournament()
     this.GetTeamsByTournament()
   },
   data() {
