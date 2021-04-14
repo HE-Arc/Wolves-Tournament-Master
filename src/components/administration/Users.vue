161b12b6c750f202d09abe5550163a061de80fee
Alan.chambaz <alan.chambaz@he-arc.ch>
2021-04-14 18:04:31 +0200

    Add users administration and RecruitDialog

---
+++ src/components/administration/Users.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="teams">
+    <v-layout style="margin:20px;">
+      <v-flex xs12>
+        <v-alert
+          class="text-xs-center"
+          v-show="loading"
+          outlined
+          v-model="loading"
+          type="info"
+        >
+          <v-progress-circular
+            indeterminate
+            color="#01002a"
+          ></v-progress-circular>
+        </v-alert>
+        <template>
+          <v-data-table
+            v-show="!loading"
+            :headers="headers"
+            :items="users"
+            sort-by="name"
+            class="elevation-1"
+          >
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-toolbar-title>USERS</v-toolbar-title>
+                <v-divider class="mx-4" inset vertical></v-divider>
+              </v-toolbar>
+            </template>
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-icon small class="mr-2" @click="UpdateTeam(item)">
+                mdi-cancel
+              </v-icon>
+            </template>
+          </v-data-table>
+        </template>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import WtmApi from '@/services/WtmApiService'
+
+export default {
+  metaInfo: {
+    title: 'Administation - Users'
+  },
+  data: () => ({
+    users: [],
+    loading: false,
+    headers: [
+      { text: 'ID', value: 'id' },
+      { text: 'Username', value: 'username' },
+      { text: 'Email', value: 'email' },
+      { text: 'Actions', value: 'actions', sortable: false }
+    ]
+  }),
+  mounted: function() {
+    this.GetUsers()
+  },
+  methods: {
+    async UpdateTeam(team) {
+      console.log(team)
+      //this.$refs.teamDialog.show(this, 'Update team', team, true)
+    },
+    async GetUsers() {
+      this.loading = true
+
+      const response = await WtmApi.Request(
+        'get',
+        this.$store.state.apiUrl + 'users/',
+        null,
+        this.$store.getters.getAxiosConfig
+      )
+
+      if (response.isSuccess) {
+        this.users = response.result
+      } else {
+        this.$snotify.error('Unable to get users...')
+      }
+
+      this.loading = false
+    }
+  }
+}
+</script>
---
+++ src/views/Administration.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="management">
+    <div>
+      <!-- Check if user is admin -->
+      <v-card height="55px" flat>
+        <v-bottom-navigation
+          v-model="bottomNav"
+          :value="true"
+          color="#01002a"
+          absolute
+        >
+          <v-btn value="Users">
+            <span>Users</span>
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+
+          <v-btn value="Teams">
+            <span>Teams</span>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+      </v-card>
+
+      <component v-bind:is="bottomNav"></component>
+    </div>
+
+    <!-- <div v-if="$userService.user.role != 'ADMIN'">
+      <v-alert :value="true" type="warning">
+        You need to be an administrator to access this page !
+      </v-alert>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import Users from '@/components/administration/Users.vue'
+import Teams from '@/components/administration/Teams.vue'
+
+export default {
+  components: {
+    Users,
+    Teams
+  },
+  data: () => ({
+    bottomNav: 'Users'
+  })
+}
+</script>
